d47f417065231b0d3533105a8839f18aa86dc80d
megansullivan19 <sullivan.megan19@gmail.com>
2026-03-24 13:31:00 -0400

    pushing back for practice

--- code/main_example.py
+++ code/main_example.py
@@ -1,5 +1,6 @@
 '''Module 3: count black and white pixels and compute the percentage of white pixels in a .jpg image'''
 #practice 
+#meg practice
 import time  # New import for timing
 from termcolor import colored
 import cv2
